da4bf24e820388465e9fce63733d1b2f0cb8a6d2
tcohen-startserverless <trevor.cohen@startserverless.dev>
2024-11-07 21:34:21 -0800

    updating workflow

--- packages/frontend/app/_layout.tsx
+++ packages/frontend/app/_layout.tsx
@@ -2,7 +2,6 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-import { client } from '@/api';
 
 
 SplashScreen.preventAutoHideAsync();
